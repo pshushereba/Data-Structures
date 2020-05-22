3b0ce32163572f7238328196ffa15c2bcd87dd51
Patrick Shushereba <pshushereba@gmail.com>
2020-05-21 20:09:33 -0700

    Finished stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/doubly_linked_list.py')
+from doubly_linked_list import DoublyLinkedList
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,16 +13,38 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         return self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) > 0:
+#             return self.storage.pop()
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size = self.size + 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        self.size -= 1
+
+        popped_value = self.storage.remove_from_tail()
+
+        return popped_value
